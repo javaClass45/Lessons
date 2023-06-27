7443a471c070989372dd7784df934689162a3772
Anton <javaClass45@gmail.com>
2023-06-27 18:28:57 +0300

    annotation-test

--- src/annotation/Exercise.java
+++ src/annotation/Exercise.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface Exercise {
+public @interface Exercise  {
     String value();
 
     Level complexityLevel() default Level.BASIC;
--- src/annotation/HelloAnnotationExercise.java
+++ src/annotation/HelloAnnotationExercise.java
@@ -1,7 +1,7 @@
 package annotation;
 
-@Deprecated
-@Exercise(value = "hello-annotation-basic")
+//@Deprecated
+@Exercise(value = "hello-annotation-basic", complexityLevel = Level.BASIC)
 public class HelloAnnotationExercise {
 
 
